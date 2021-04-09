477058be4ed417f93de5252f1ba208f6ae2553ea
Admin <andres.fso@hotmail.com>
2021-04-09 09:28:13 -0500

    listo

--- src/app/shared/components/document-viewer/document-viewer.component.ts
+++ src/app/shared/components/document-viewer/document-viewer.component.ts
@@ -8,11 +8,11 @@ import { DxReportViewerModule } from 'devexpress-reporting-angular';
   encapsulation: ViewEncapsulation.None,
   templateUrl: './document-viewer.component.html',
   styleUrls: [
-    "../../../../../node_modules/jquery-ui/themes/base/all.css",
-    "../../../../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
-    "../../../../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css",
-    "../../../../../node_modules/devexpress-reporting/dist/css/dx-webdocumentviewer.css"
-]
+    '../../../../../node_modules/jquery-ui/themes/base/all.css',
+    '../../../../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css',
+    '../../../../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css',
+    '../../../../../node_modules/devexpress-reporting/dist/css/dx-webdocumentviewer.css',
+  ],
 })
 export class DocumentViewerComponent {
   @Input()
